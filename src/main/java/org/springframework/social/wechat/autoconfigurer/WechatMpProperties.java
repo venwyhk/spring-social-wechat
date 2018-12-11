bde9028bde89327fb77db9d093c423e196176a33
Larry <larry7696@gmail.com>
2018-12-11 15:35:34 +1300

    bugfix

--- src/main/java/org/springframework/social/wechat/autoconfigurer/WechatMpProperties.java
+++ src/main/java/org/springframework/social/wechat/autoconfigurer/WechatMpProperties.java
@@ -3,6 +3,9 @@ package org.springframework.social.wechat.autoconfigurer;
 import org.springframework.boot.autoconfigure.social.SocialProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * spring-social-wechat
  * 
@@ -10,4 +13,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "spring.social.wechatmp")
 public class WechatMpProperties extends SocialProperties {
+
+	@Getter
+	@Setter
+	private String scope = "snsapi_userinfo";
+
 }
