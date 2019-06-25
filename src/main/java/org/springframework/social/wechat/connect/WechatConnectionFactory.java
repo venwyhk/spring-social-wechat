1fbf035e3ce218666f0d10cd412ff3c944226831
Larry <larry7696@gmail.com>
2019-06-25 19:04:02 +0800

    1.3.2

--- src/main/java/org/springframework/social/wechat/connect/WechatConnectionFactory.java
+++ src/main/java/org/springframework/social/wechat/connect/WechatConnectionFactory.java
@@ -4,21 +4,23 @@ import org.springframework.social.connect.ApiAdapter;
 import org.springframework.social.connect.support.OAuth2ConnectionFactory;
 import org.springframework.social.oauth2.AccessGrant;
 
-import org.springframework.social.wechat.api.Wechat;
+import org.springframework.social.wechat.UrlConstants;
+import org.springframework.social.wechat.api.WechatMp;
 
 /**
  * spring-social-wechat
  * 
  * @author Larry
  */
-public class WechatConnectionFactory extends OAuth2ConnectionFactory<Wechat> {
+public class WechatMpConnectionFactory extends OAuth2ConnectionFactory<WechatMp> {
 
-	public WechatConnectionFactory(String appId, String appSecret) {
-		this(appId, appSecret, new WechatAdapter<Wechat>());
+	public WechatMpConnectionFactory(String appId, String appSecret) {
+		this(appId, appSecret, new WechatAdapter<WechatMp>());
 	}
 
-	public WechatConnectionFactory(String appId, String appSecret, ApiAdapter<Wechat> apiAdapter) {
-		super("wechat", new WechatServiceProvider<Wechat>(appId, appSecret), apiAdapter);
+	public WechatMpConnectionFactory(String appId, String appSecret, ApiAdapter<WechatMp> apiAdapter) {
+		super("wechatmp", new WechatServiceProvider<WechatMp>(appId, appSecret, UrlConstants.AUTHORIZE_API_URL),
+				apiAdapter);
 	}
 
 	@Override
