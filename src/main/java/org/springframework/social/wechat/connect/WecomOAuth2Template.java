2bed5d87a423d140f0ff64f16c03810fd97950fb
Larry <larry7696@gmail.com>
2022-09-13 19:08:33 +0800

    V1.3.3

--- src/main/java/org/springframework/social/wechat/connect/WecomOAuth2Template.java
+++ src/main/java/org/springframework/social/wechat/connect/WecomOAuth2Template.java
@@ -1,6 +1,8 @@
 package org.springframework.social.wechat.connect;
 
+import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.util.MultiValueMap;
 
 import lombok.Setter;
 
@@ -16,6 +18,17 @@ public class WecomOAuth2Template extends WechatOAuth2Template {
 
 	public WecomOAuth2Template(String clientId, String clientSecret, String authorizeUrl, String accessTokenUrl) {
 		super(clientId, clientSecret, authorizeUrl, accessTokenUrl);
+	}
+	
+	@Override
+	protected AccessGrant postForAccessGrant(String accessTokenUrl, MultiValueMap<String, String> parameters) {
+		if ("authorization_code".equals(parameters.getFirst("grant_type"))) {
+			parameters.set("corpid", parameters.getFirst("client_id"));
+			parameters.remove("client_id");
+			parameters.set("corpsecret", parameters.getFirst("client_secret"));
+			parameters.remove("client_secret");
+		}
+		return super.postForAccessGrant(accessTokenUrl, parameters);
 	}
 
 	@Override
